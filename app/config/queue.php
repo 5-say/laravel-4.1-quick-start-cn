689e85dd87c3dca14f711d9f851f0ffb81374a18
bcw <bcw.5@foxmail.com>
2014-06-01 17:13:16 +0800

    队列配置 翻译结束

--- app/config/queue.php
+++ app/config/queue.php
@@ -5,13 +5,18 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Default Queue Driver
+	| 默认队列驱动
 	|--------------------------------------------------------------------------
 	|
 	| The Laravel queue API supports a variety of back-ends via an unified
+	| Laravel 队列 API 支持多种后端通过一个统一的 API 进行调用，
 	| API, giving you convenient access to each back-end using the same
+	| 给予你相同的语法来方便的访问每一个后端。
 	| syntax for each one. Here you may set the default queue driver.
+	| 这里你可以设置默认的队列驱动。
 	|
 	| Supported: "sync", "beanstalkd", "sqs", "iron"
+	| 支持: "sync", "beanstalkd", "sqs", "iron"
 	|
 	*/
 
@@ -20,11 +25,15 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Queue Connections
+	| 队列连接
 	|--------------------------------------------------------------------------
 	|
 	| Here you may configure the connection information for each server that
+	| 在这里，你可以为每一个，你应用程序所使用的服务器，配置连接信息。
 	| is used by your application. A default configuration has been added
+	| 一个默认的配置已经添加，
 	| for each back-end shipped with Laravel. You are free to add more.
+	| 为每一个 Laravel 所支持的后端。你可以自由的追加更多的配置。
 	|
 	*/
 
@@ -67,11 +76,15 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Failed Queue Jobs
+	| 失败队列作业
 	|--------------------------------------------------------------------------
 	|
 	| These options configure the behavior of failed queue job logging so you
+	| 这些选项配置了失败队列作业的日志记录行为，因此你
 	| can control which database and table are used to store the jobs that
+	| 可以控制使用哪个数据库和表来存储这些失败的作业。
 	| have failed. You may change them to any database / table you wish.
+	| 你可以改变他们为任何你想要的数据库与表。
 	|
 	*/
 
