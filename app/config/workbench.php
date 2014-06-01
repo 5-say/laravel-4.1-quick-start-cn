840166957a51923adbc483e5169e5be5aa7e0032
bcw <bcw.5@foxmail.com>
2014-06-01 10:10:42 +0800

    工作台配置文件 翻译结束

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -5,11 +5,15 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Workbench Author Name
+	| 工作台作者姓名
 	|--------------------------------------------------------------------------
 	|
 	| When you create new packages via the Artisan "workbench" command your
+	| 当你通过 Artisan 的 "workbench" 命令创建新的资源包，
 	| name is needed to generate the composer.json file for your package.
+	| 你的名字是必要的，在为你的资源包生成 composer.json 文件时。
 	| You may specify it now so it is used for all of your workbenches.
+	| 你可以现在指定它，这样它将被用于你所有的工作台。
 	|
 	*/
 
@@ -18,11 +22,15 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Workbench Author E-Mail Address
+	| 工作台 作者 E-Mail 地址
 	|--------------------------------------------------------------------------
 	|
 	| Like the option above, your e-mail address is used when generating new
+	| 如同上一个选项，当生成新的工作台资源包时你的 e-mail 地址将被使用。
 	| workbench packages. The e-mail is placed in your composer.json file
+	| 这个 e-mail 是
 	| automatically after the package is created by the workbench tool.
+	| 在 workbench 工具创建资源包后，放入你的 composer.json 文件中的。
 	|
 	*/
 
